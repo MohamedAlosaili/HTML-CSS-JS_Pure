a25065b6a0405b1a0fee04becc611ad0cce049f7
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-04-22 01:04:25 +0300

    Another Solution

--- Blurry Loading/script.js
+++ Blurry Loading/script.js
@@ -9,3 +9,25 @@ function increaseNumber() {
   if (number === 100) clearInterval(increasePrec);
   else loading.innerHTML = ++number;
 }
+
+// This A Great Solve I See It On Github It's Refer To Question On StackOverFlow
+// // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+
+/*
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(int);
+  }
+
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min; // (0 - 0) * (0 - 1)
+};
+console.log(((55 - 0) * (0 - 1)) / (100 - 0) + 1);
+*/
