88a7f85b23dd22031bc33e8088ef90d422f43abd
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-05-18 21:27:08 +0300

    Added Content Placeholder

---
+++ Content-Placeholder/script.js
@@ -0,0 +1,10 @@
+const cardTemplate = document.querySelector("#card-template");
+const skeletonTemplate = document.querySelector("#skeleton-template");
+const container = document.querySelector("#cards-holder");
+
+container.append(skeletonTemplate.content.cloneNode(true));
+
+// setTimeout(() => {
+//   container.innerHTML = "";
+//   container.append(cardTemplate.content.cloneNode(true));
+// }, 2000);
